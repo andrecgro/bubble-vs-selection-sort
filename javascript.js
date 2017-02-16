957871fc0904b93a72adcacff73e9d557ff5c773
André Rocha <andre.cgrocha@gmail.com>
2017-02-15 22:49:00 -0200

    duplicating selection sort to test

--- javascript.js
+++ javascript.js
@@ -1,34 +1,59 @@
-function animatedBubbleSort(items, drawCallback) {
+function animatedBubbleSort(bubble, selection, drawBubble, drawSelection) {
   var i = 0;
   var j = 0;
-  var length = items.length;
-  (function nextIteration() {
+  var k = 0;
+  var l = 0;
+  var steps = 0;
+  var length = bubble.length;
+  (function selectionSort() {
+    if (l >= length - k - 1) {
+      l = 0;
+      k++;
+    }
+    if (k < length) {
+      if (selection[l] > selection[l+1]) {
+        // swap itens
+        var temp = selection[l];
+        selection[l] = selection[l+1];
+        selection[l+1] = temp;
+        drawSelection(selection, l+1);
+      }
+      l++;
+      setTimeout(bubbleSort, 100);
+    } else //fim
+      drawSelection(selection);
+  })();
+  (function bubbleSort() {
     if (j >= length - i - 1) {
       j = 0;
       i++;
     }
     if (i < length) {
-      if (items[j] > items[j+1]) {
+      if (bubble[j] > bubble[j+1]) {
         // swap itens
-        var temp = items[j];
-        items[j] = items[j+1];
-        items[j+1] = temp;
-        drawCallback(items, j+1);
+        var temp = bubble[j];
+        bubble[j] = bubble[j+1];
+        bubble[j+1] = temp;
+        drawBubble(bubble, j+1);
       }
       j++;
-      setTimeout(nextIteration, 100);
+      setTimeout(selectionSort, 100);
     } else //fim
-      drawCallback(items);
+      drawBubble(bubble);
   })();
 }
 
 function ordenaRandom(){
-  var list = [];
+  var bubble = [];
+  var selection = [];
   for (var i = 0; i < 20; i++) {
-    list.push(Math.floor((Math.random() * 100) + 1));
+    bubble.push(Math.floor((Math.random() * 100) + 1));
   }
-  animatedBubbleSort(list, function draw(items, j) {
-    document.getElementById("bubble-sort").innerHTML = items.map(function(v, i) { return i===j ? "<span>" + v + "</span>" : v; }).join(", ");
+  selection = bubble;
+  animatedBubbleSort(bubble, selection, function draw(bubble, j) {
+    document.getElementById("bubble-sort").innerHTML = bubble.map(function(v, i) { return i===j ? "<span>" + v + "</span>" : v; }).join(", ");
+  }, function draw(selection, l) {
+    document.getElementById("selection-sort").innerHTML = selection.map(function(s, k) { return k===l ? "<span>" + s + "</span>" : s; }).join(", ");
   });
 }
 
@@ -41,9 +66,6 @@ function ordenaCustom(){
   for (var i = 0; i < listAux.length; i++) {
     list.push(parseInt(listAux[i]));
   }
-  animatedBubbleSort(list, function draw(items, j) {
-    document.getElementById("bubble-sort").innerHTML = items.map(function(v, i) { return i===j ? "<span>" + v + "</span>" : v; }).join(", ");
-  });
 }
 
 //alert("Você pode escolher entre ordenar uma lista de valores randômicos, ou uma lista com elementos de sua escolha!");
